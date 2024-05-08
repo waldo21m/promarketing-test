18f90b4fd6499ede43559eebb1c868f2a5673a23
Eduardo Márquez <eduardo20.3263@gmail.com>
2024-05-08 13:20:30 -0400

    Provider mock and types with normalize providers function

---
+++ src/mock/provider.mock.ts
@@ -0,0 +1,59 @@
+import { type Provider } from '../types/providerTypes';
+
+export const providersMock: Provider[] = [
+	{
+		id: 1,
+		name: 'Slots',
+		description: 'Slots',
+	},
+	{
+		id: 2,
+		name: 'Keno',
+		description: 'Bingo',
+	},
+	{
+		id: 3,
+		name: 'Baccarat',
+		description: 'Baccarat',
+	},
+	{
+		id: 4,
+		name: 'Roulette',
+		description: 'Ruleta',
+	},
+	{
+		id: 5,
+		name: 'Blackjack',
+		description: 'Blackjack',
+	},
+	{
+		id: 6,
+		name: 'Poker',
+		description: 'Poker',
+	},
+	{
+		id: 7,
+		name: 'Otros',
+		description: 'Otros',
+	},
+	{
+		id: 8,
+		name: 'Sportsbook',
+		description: 'Sportsbook',
+	},
+	{
+		id: 9,
+		name: 'Quiniela',
+		description: 'Cartilla',
+	},
+	{
+		id: 10,
+		name: 'Alabama',
+		description: 'Lacey',
+	},
+	{
+		id: 11,
+		description: 'Daytona Beach',
+		name: 'Louisiana',
+	},
+];
---
+++ src/types/providerTypes.ts
@@ -0,0 +1,14 @@
+export interface ProviderBase {
+	name: string;
+	description: string;
+}
+
+export interface ProviderAPI extends ProviderBase {
+	id: number | string;
+	Name?: string;
+	Descripcion?: string;
+}
+
+export interface Provider extends ProviderBase {
+	id: number;
+}
